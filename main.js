b240b0ece0ec8ef875cb31a8c42cf50910af0e76
viniciussoares7 <vinicius-a-soares@outlook.com>
2021-06-23 00:07:14 -0300

    Included JavaScript and somefeatures :sparkles:

---
+++ main.js
@@ -0,0 +1,28 @@
+/* ABRE/FECHA do menu ao clicar no icone: hamburger ou x*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* Esconder menu ao clicar em um item do menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+/* mudar o header da pagina (scroll) */
+const header = document.querySelector('header')
+const navHeight = header.offsetHeight
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
