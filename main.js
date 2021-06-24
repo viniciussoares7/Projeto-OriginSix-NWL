bd81d4172bac70cd1164c217961325203213cc71
viniciussoares7 <vinicius-a-soares@outlook.com>
2021-06-24 18:04:51 -0300

    Added social media links (html & css), Added button 'back-to-top' (html, css & JavaScript), Added new Icon to Assets file

--- main.js
+++ main.js
@@ -9,7 +9,9 @@ for (const element of toggle) {
 }
 
 /* Esconder menu ao clicar em um item do menu */
-const links = document.querySelectorAll('nav ul li a')
+const links = document.querySelectorAll(
+  'nav ul li a, footer .logo, .back-to-top'
+)
 
 for (const link of links) {
   link.addEventListener('click', function (event) {
@@ -19,16 +21,27 @@ for (const link of links) {
     scrollSmooth(link)
   })
 }
+/* Smooth Scroll */
+
+function scrollSmooth(link) {
+  const sectionId = link.getAttribute('href')
+  document.querySelector(sectionId).scrollIntoView({
+    behavior: 'smooth'
+  })
+}
+
 /* mudar o header da pagina (scroll) */
-const header = document.querySelector('header')
-const navHeight = header.offsetHeight
-window.addEventListener('scroll', function () {
+
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('header')
+  const navHeight = header.offsetHeight
+
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 /* Testimonial carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
@@ -40,21 +53,12 @@ const swiper = new Swiper('.swiper-container', {
   keyboard: true
 })
 
-/* Smooth Scroll */
-
-function scrollSmooth(link) {
-  const sectionId = link.getAttribute('href')
-  document.querySelector(sectionId).scrollIntoView({
-    behavior: 'smooth'
-  })
-}
-
 /* ScrollReveal: Mostrar elementos quando der scroll na página */
 
 const scrollReveal = ScrollReveal({
   origin: 'top',
   distance: '30px',
-  duration: 700,
+  duration: 500,
   reset: true
 })
 
@@ -64,7 +68,23 @@ scrollReveal.reveal(
 #about .image, #about .text,
 #services header, #services .card,
 #testimonials header, #testimonials .testimonials
-#contact .text, #contact .links
+#contact .text, #contact .links, footer .brand, footer .social
 `,
   { interval: 100 }
 )
+
+/* back to top button */
+function backtoTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backtoTop()
+})
